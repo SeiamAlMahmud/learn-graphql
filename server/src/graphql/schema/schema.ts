75642b09a91914f2dd817882ebfb201d539c1aa7
SeiamAlMahmud <samahmud49@gmail.com>
2024-12-21 12:30:35 +0600

    first commit

---
+++ server/src/graphql/schema/schema.ts
@@ -0,0 +1,20 @@
+export const schema = `#graphql
+
+type User {
+    _id: ID!,
+    name: String!,
+    email: String!,
+    password: String!,
+    googleId: String!,
+    role: String!,
+    avatar: String!,
+    verified: Boolean!,
+    createdAt: String!,
+    updatedAt: String!
+}
+
+type Query {
+    hello: String
+    users: [User]
+}
+`
---
+++ server/src/models/user.model.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    googleId: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin', 'instructor'],
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    verified: {
+        type: Boolean,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['male', 'female'],
+        required: true
+    },
+    // watching: {
+    //     typr: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Course'
+    // },
+    // saved: {
+    //     typr: mongoose.Schema.Types.ObjectId,
+    //     ref: 'Course'
+    // },
+    verificationToken: {type: String},
+    verificationExpire: {type: Date},
+
+    passwordToken: {type: String},
+    passwordExpire: {type: Date},
+},{timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
